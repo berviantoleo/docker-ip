8b146bc551791c8b8721080bcc816d1c86c1ba45
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-05-30 13:31:54 +0700

    update format

--- infra/main.tf
+++ infra/main.tf
@@ -20,15 +20,15 @@ provider "aws" {
 
 
 resource "aws_apprunner_service" "docker_ip" {
-    service_name = "docker-ip"
-    source_configuration {
-        image_repository {
-            image_configuration {
-                port = "80"
-            }
-            image_identifier = "public.ecr.aws/nginx/nginx:1.24-alpine-slim"
-            image_repository_type = "ECR_PUBLIC"
-        }
-        auto_deployments_enabled = false
+  service_name = "docker-ip"
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "80"
+      }
+      image_identifier      = "public.ecr.aws/nginx/nginx:1.24-alpine-slim"
+      image_repository_type = "ECR_PUBLIC"
     }
+    auto_deployments_enabled = false
+  }
 }
